0928fa867e6e0af9f29939033cd01d2c335a7cc2
suchaj jongprasit <jongzazaal@gmail.com>
2015-12-23 10:50:00 +0700

    เพิ่มanimationการเดิน

--- Raise Life (nsc18)/Assets/Script/control_Player.cs
+++ Raise Life (nsc18)/Assets/Script/control_Player.cs
@@ -8,21 +8,31 @@ public class control_Player : MonoBehaviour {
 	public static control_Player instance;
 	public Joystick a;
 	public Transform rect; 
+	Animator anim;
 	void Awake (){
 		instance = this;
 		rect = gameObject.GetComponent<Transform> ();
 		a = GameObject.Find ("MobileSingleStickControl").transform.FindChild ("MobileJoystick").GetComponent<Joystick> ();
 	}
 	void Start () {
-
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(a.moveX != 0)
-			rect.Translate(0.0005f * a.moveX, 0, 0);
-		if(a.moveY != 0)
-			rect.Translate(0, 0.0005f * a.moveY, 0);
+		if (a.moveX != 0) {
+			rect.Translate (0.0008f * a.moveX, 0, 0);
+			anim.SetBool("iswalking", true);
+			anim.SetFloat ("input_x", a.moveX);
+		}
+		if (a.moveY != 0) {
+			rect.Translate (0, 0.0008f * a.moveY, 0);
+			anim.SetBool("iswalking", true);
+			anim.SetFloat ("input_y", a.moveY);
+		}
+		if (a.moveX == 0 && a.moveY == 0) {
+			anim.SetBool("iswalking", false);
+		}
 		//print (a.moveX+"");
 		//print ("a");
 	}
