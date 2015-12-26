773fb49d97741486441d46f4bc2eeb1c45b9b70b
suchaj jongprasit <jongzazaal@gmail.com>
2015-12-26 16:45:52 +0700

    เพิ่มอนิเมชั่นวัว

--- Raise Life (nsc18)/Assets/Script/ai.cs
+++ Raise Life (nsc18)/Assets/Script/ai.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 
 public class ai : MonoBehaviour {
-	public Transform rect; 
+	public Transform rect;
+	public int id = 2;
+	public GameObject objToSpawn;
 	// Use this for initialization
 	void Start () {
 		rect = gameObject.GetComponent<Transform> ();
+		for (int i = 0; i < 5; i++) {
+			objToSpawn = new GameObject("Cool GameObject made from Code");
+			//Add Components
+			objToSpawn.AddComponent<Rigidbody2D>();
+			objToSpawn.AddComponent<MeshFilter>();
+			objToSpawn.AddComponent<BoxCollider>();
+			objToSpawn.AddComponent<MeshRenderer>();
+			objToSpawn.AddComponent<SpriteRenderer>();
+			objToSpawn.transform.position = new Vector2(0,0);
+
+
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		rect.Translate (0, -0.01f, 0);
+		//gameObject.GetComponent<Rigidbody2D> ().AddForce (Physics.gravity * 0);
 	}
 }
