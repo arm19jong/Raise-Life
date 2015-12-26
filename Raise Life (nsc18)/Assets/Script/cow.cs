623bf6356dfe6ec58bcd383aa2bb9643fed288ad
suchaj jongprasit <jongzazaal@gmail.com>
2015-12-26 17:57:49 +0700

    random walk cow

---
+++ Raise Life (nsc18)/Assets/Script/cow.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cow : MonoBehaviour {
+	float timeLeft = 0.0f;
+	public Transform rect;
+	public static cow instance;
+	int randomxy = 0;
+	int randomint = 0;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		instance = this;
+		rect = gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+
+		if (timeLeft <= 0) {
+			//print ("e");
+			timeLeft = 4.0f;
+			anim.SetBool("iswalking", false);
+			randomxy = Random.Range(0,2);
+
+			if (randomxy==0)
+			{
+				while(randomint==0){
+				randomint = Random.Range(-1, 2);
+				}
+				anim.SetFloat ("input_x", randomint);
+				anim.SetFloat ("input_y", 0);
+
+			}
+			else
+			{
+				while(randomint==0){
+					randomint = Random.Range(-1, 2);
+				}
+				anim.SetFloat ("input_y", randomint);
+				anim.SetFloat ("input_x", 0);
+
+			}
+
+
+
+		} 
+		//walk
+		if (timeLeft < 2) 
+		{
+			anim.SetBool("iswalking", true);
+			//anim.SetFloat ("input_x", 1);
+			//rect.Translate ( 0.01f, 0, 0);
+			if(randomxy==0)
+			{
+				rect.Translate ( 0.01f*randomint, 0, 0);
+			}
+			else
+			{
+				rect.Translate ( 0, 0.01f*randomint, 0);
+			}
+		}
+		//stop
+		else 
+		{
+			anim.SetBool("iswalking", false);
+			//anim.SetFloat ("input_x", 0);
+		}
+	}
+}
