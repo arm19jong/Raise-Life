8c986c3ed5400390dce375a3fc07ed22bb372e74
suchaj jongprasit <jongzazaal@gmail.com>
2015-12-30 18:03:58 +0700

    test map

--- Raise Life (nsc18)/Assets/Script/prefab.cs
+++ Raise Life (nsc18)/Assets/Script/prefab.cs
@@ -42,7 +42,7 @@ public class prefab : MonoBehaviour {
 		print (222);
 		//Object prefab = AssetDatabase.LoadAssetAtPath("Assets/prefab/cow.prefab", typeof(GameObject));
 		print(333);
-		GameObject clone = Instantiate(Resources.Load("cow"), Vector3.zero, Quaternion.identity) as GameObject;
+		GameObject clone = Instantiate(Resources.Load("cow1"), Vector3.zero, Quaternion.identity) as GameObject;
 		print (444);
 		clone.transform.position = new Vector3(5, 2, 0);
 		print(555);
