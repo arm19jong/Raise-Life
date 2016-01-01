f61a1294c4af30439deb8077d81882d999973617
suchaj jongprasit <jongzazaal@gmail.com>
2016-01-01 17:55:49 +0700

    แสงเงา

---
+++ Raise Life (nsc18)/Assets/Script/light.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent( typeof( Light ) )]
+public class light : MonoBehaviour {
+	float a=0;
+	GameObject lightGameObject ;
+	Light lightComp;
+	
+	float newS, sMin, sMax, newMin = 0, newMax = 1;
+
+	void Start () {
+		lightGameObject = new GameObject("Directional lightuuuuu");
+		lightComp = lightGameObject.AddComponent<Light>();
+		lightComp.type = LightType.Directional;
+	}
+	void Update () {
+		a += Time.deltaTime;
+		if (a >= 20) {
+			a = 0;
+		} 
+		else if (a >= 10) {
+			sMin = 10;
+			sMax = 19;
+			newS =  (((a - sMin) * (newMax - newMin)) / (sMax - sMin)) + newMin;
+			lightComp.color = Color.Lerp (new Color(1,0.171f, 1,1),Color.white, newS);
+			lightGameObject.transform.position = new Vector3(8, 2, -1);
+		}
+		else{
+			sMin = 0;
+			sMax = 9;
+			newS =  (((a - sMin) * (newMax - newMin)) / (sMax - sMin)) + newMin;
+			lightComp.color = Color.Lerp (Color.white,new Color(1,0.171f, 1,1), newS);
+			lightGameObject.transform.position = new Vector3(8, 2, -1);
+		}
+		print (a);
+	}
+	void redtowhite(){
+		lightComp.color = Color.Lerp (new Color(1,0.171f, 1, 1), Color.white,9.9f);
+		lightGameObject.transform.position = new Vector3(8, 2, -1);
+	}
+	void whitetored(){
+		print (lightComp.color);
+		//if (lightComp.color.r == 1 && lightComp.color.g==0.171 && lightComp.color.b==1) {
+		//	return;
+		//}
+		lightComp.color = Color.Lerp (Color.white,new Color(1,0.171f, 1,1), 9f);
+		lightGameObject.transform.position = new Vector3(8, 2, -1);
+	}
+
+
+}
--- Raise Life (nsc18)/Assets/Script/time.cs
+++ Raise Life (nsc18)/Assets/Script/time.cs
@@ -4,14 +4,34 @@ using System.Collections;
 public class time : MonoBehaviour {
 	private GUIStyle guiStyle = new GUIStyle(); //create a new variable
 	float a=0;
+	int min = 0;
+	int hours = 6;
+	string s_min = "";
+	string s_hours = "";
 	void OnGUI()
 	{
-		guiStyle.fontSize = 20;
-		guiStyle.normal.textColor = Color.red;
-		GUI.Label(new Rect(Screen.width-110, 0, 110, 50),"ฟฟฟห"+"\n"+a,  guiStyle);
+		guiStyle.fontSize = 25;
+		Font myFont = (Font)Resources.Load("Let's go Digital Regular", typeof(Font));
+		guiStyle.font = myFont;
+		//guiStyle
+		guiStyle.normal.textColor = Color.black;
+		s_min = min.ToString("00");
+		s_hours = hours.ToString ("00");
+		GUI.Label(new Rect(Screen.width-110, 5, 110, 50),"AaBbCc"+"\n"+s_hours+":"+s_min,  guiStyle);
 	}
 	void FixedUpdate()
 	{
 		a += Time.deltaTime;
+		if (a>=10){
+			a=0;
+			min+=10;
+		}
+		if (min>=60){
+			min=0;
+			hours+=1;
+		}
+		if(hours>=24){
+			hours=0;
+		}
 	}
 }
