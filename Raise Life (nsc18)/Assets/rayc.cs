7ad275b5abde98ba9f66d916ee870b3949375f9f
suchaj jongprasit <jongzazaal@gmail.com>
2016-01-17 19:47:15 +0700

    fig

---
+++ Raise Life (nsc18)/Assets/rayc.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rayc : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		Vector2 fwd = transform.TransformDirection(Vector2.);
+		if (Physics.Raycast(transform.position, fwd, 10)) {
+			print("There is something in front of the object!");
+		}
+		*/
+	}
+}
